ab1d0045ab1c96200771f50b6712449a36c798b7
Sahasi52 <siamsadikhasan52@gmail.com>
2025-07-04 15:09:33 +0600

    Update all project files

--- my-app/src/data/WorkData.js
+++ my-app/src/data/WorkData.js
@@ -32,31 +32,20 @@ export const Work = [
   },
   {
     id: 4,
-    name: "Crop & Fertilizer Recommendation System Website",
+    name: "Portfolio Website",
     description:
-      "A full-stack website for recommending crops and fertilizers using React, Tailwind, Node.js, and MySQL.",
-    tags: ["React", "Tailwind CSS", "Node.js", "MySQL"],
-    demo: "https://agrisense-web.netlify.app/",
-    github:
-      "https://github.com/Sahasi52/Crop-Fertilizer-Recommendation-System-web",
+      "A minimal React portfolio website using Framer Motion for smooth animations and Styled-Components for clean, component-based styling.",
+    tags: ["React", "JavaScript"],
+    demo: "https://portfolio-webbb.netlify.app/",
+    github: "https://github.com/Sahasi52/Portfolio-web",
   },
   {
     id: 5,
-    name: "Crop & Fertilizer Recommendation System Website Demo",
+    name: "Weather Forecast Website",
     description:
-      "A static demo site using HTML, CSS, and JavaScript with basic form validation and UI features.",
+      "A simple, responsive weather app using HTML, CSS, and JavaScript that shows real-time weather for any city using the OpenWeatherMap API.",
     tags: ["HTML", "CSS", "JavaScript"],
-    demo: "https://agrisense-web-demo.netlify.app/",
-    github:
-      "https://github.com/Sahasi52/Crop-Fertilizer-Recommendation-System-web-demo",
-  },
-  {
-    id: 6,
-    name: "Ecommerce Website",
-    description:
-      "A simple and responsive e-commerce frontend using HTML, CSS, and JavaScript.",
-    tags: ["HTML", "CSS", "JavaScript"],
-    demo: "https://ecommerce-webb.netlify.app/",
-    github: "https://github.com/Sahasi52/Ecommerce-web",
+    demo: "https://weather-forecast-webb.netlify.app/",
+    github: "https://github.com/Sahasi52/Weather-Forecast-web",
   },
 ];
